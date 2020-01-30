d03718f035c583ac1cc9bed2f59c9312bb2ba6bd
vitasgit <vitasanon@mail.ru>
2020-01-30 19:40:27 +0500

    add interface
    pizdez

---
+++ Fly2/FlyingGame/mainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FlyingGame
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRules = new System.Windows.Forms.Button();
+            this.buttonSetting = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRules
+            // 
+            this.buttonRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRules.Location = new System.Drawing.Point(562, 94);
+            this.buttonRules.Name = "buttonRules";
+            this.buttonRules.Size = new System.Drawing.Size(175, 57);
+            this.buttonRules.TabIndex = 0;
+            this.buttonRules.Text = "Правила игры";
+            this.buttonRules.UseVisualStyleBackColor = true;
+            // 
+            // buttonSetting
+            // 
+            this.buttonSetting.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSetting.Location = new System.Drawing.Point(562, 208);
+            this.buttonSetting.Name = "buttonSetting";
+            this.buttonSetting.Size = new System.Drawing.Size(175, 57);
+            this.buttonSetting.TabIndex = 1;
+            this.buttonSetting.Text = "Настройка игры";
+            this.buttonSetting.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlay.Location = new System.Drawing.Point(562, 325);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(175, 57);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Начать играть";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click_1);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonSetting);
+            this.Controls.Add(this.buttonRules);
+            this.Name = "mainForm";
+            this.Text = "Игра \"Сбей самолёт\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRules;
+        private System.Windows.Forms.Button buttonSetting;
+        private System.Windows.Forms.Button buttonPlay;
+    }
+}
---
+++ Fly2/FlyingGame/mainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyingGame
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlay_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("asd");
+        }
+    }
+}
